5eeea7a6b0bd393ad1227d1517e2a63accc4b833
suu3 <ahob789@ewhain.net>
2023-09-25 04:58:42 +0900

    [FIX] 카테고리 active

--- src/components/@layout/category-menu.tsx
+++ src/components/@layout/category-menu.tsx
@@ -4,7 +4,7 @@ import { list, active, inactive } from "./category-menu.module.css"
 import { withPrefix } from "gatsby"
 
 const CategoryMenu = ({ categories = {}, pathname = "" }) => {
-  const [clicked, setClicked] = useState(pathname)
+  const [clicked, setClicked] = useState(`/${pathname.split("/")[1]}`)
   const { totalCount, group } = categories
 
   const listClassName = (category: string) =>
