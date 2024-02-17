55fa80c96ad58f5c8f86882d8925990975641cd6
suu3 <dev.suu3@gmail.com>
2024-02-18 02:39:29 +0900

    [style] 일반 메인으로 변경

--- src/components/@layout/transition-main.tsx
+++ src/components/@layout/transition-main.tsx
@@ -9,29 +9,29 @@ const TransitionMain = ({
   className?: string
 }) => {
   return (
-    <motion.main
-      className={className}
-      initial={{
-        opacity: 0,
-        x: -200,
-      }}
-      animate={{
-        opacity: 1,
-        x: 0,
-      }}
-      exit={{
-        opacity: 0,
-        x: 200,
-      }}
-      transition={{
-        type: "spring",
-        mass: 0.35,
-        stiffness: 75,
-        duration: 0.15,
-      }}
-    >
-      {children}
-    </motion.main>
+    // <motion.main
+    //   className={className}
+    //   initial={{
+    //     opacity: 0,
+    //     x: -200,
+    //   }}
+    //   animate={{
+    //     opacity: 1,
+    //     x: 0,
+    //   }}
+    //   exit={{
+    //     opacity: 0,
+    //     x: 200,
+    //   }}
+    //   transition={{
+    //     type: "spring",
+    //     mass: 0.35,
+    //     stiffness: 75,
+    //     duration: 0.15,
+    //   }}
+    // >
+    <main className={className}>{children}</main>
+    // </motion.main>
   )
 }
 
