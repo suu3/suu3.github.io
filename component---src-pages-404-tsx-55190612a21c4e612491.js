40fbac302094c14371953cc0e5083f95599007da
suu3 <suu3@users.noreply.github.com>
2024-03-12 23:14:16 +0000

    Automated deployment: Tue Mar 12 23:14:16 UTC 2024 090a0ddb591786afe7d864ce5e812fa3dea50bf0

---
+++ component---src-pages-404-tsx-55190612a21c4e612491.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_blog=self.webpackChunkgatsby_blog||[]).push([[218],{9233:function(t,e,n){n.r(e),n.d(e,{Head:function(){return c}});var l=n(3236),a=n(5541),o=n(3777);const c=()=>l.createElement(o.Z,{title:"404: Not Found"});e.default=t=>{let{data:e,location:n}=t;const o=e.site.siteMetadata.title;return l.createElement(a.Z,{location:n,title:o},l.createElement("h1",null,"404: Not Found"),l.createElement("p",null,"존재하지 않는 페이지입니다."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-55190612a21c4e612491.js.map
