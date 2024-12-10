67403af7d799f47b1e21f035f51750f1dbebc396
suu3 <dev.suu3@gmail.com>
2024-12-11 02:35:18 +0900

    Updates

---
+++ component---src-pages-404-tsx-86f9fa968fac67d0fa5d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_blog=self.webpackChunkgatsby_blog||[]).push([[218],{6677:function(t,e,n){n.r(e),n.d(e,{Head:function(){return c}});var l=n(3236),a=n(3304),o=n(3231);const c=()=>l.createElement(o.Z,{title:"404: Not Found"});e.default=t=>{let{data:e,location:n}=t;const o=e.site.siteMetadata.title;return l.createElement(a.Z,{location:n,title:o},l.createElement("h1",null,"404: Not Found"),l.createElement("p",null,"존재하지 않는 페이지입니다."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-86f9fa968fac67d0fa5d.js.map
